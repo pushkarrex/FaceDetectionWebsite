69092c1d96a17546dfae458809ba068c72ff7fb1
pushkarrex <pushkarmidex@gmail.com>
2019-09-13 16:31:17 +0530

    deploy

--- facedetectionwebsite/src/App.js
+++ facedetectionwebsite/src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
         this.state.input)
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://cryptic-stream-76926.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
